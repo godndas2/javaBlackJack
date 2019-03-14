0252a4423ff92d021f03087a6be55f38ca593a59
godndas2 <gusgodnd@naver.com>
2019-03-14 15:32:31 +0900

    initPhase() 생성

--- javaBlackJack/src/main/java/domain/Game.java
+++ javaBlackJack/src/main/java/domain/Game.java
@@ -1,14 +1,48 @@
 package domain;
 
+import java.util.Scanner;
+
 public class Game {
 
 	public void play() {
 		System.out.println("========== BlackJack ==========");
+		Scanner sc = new Scanner(System.in);
 		Dealer dealer = new Dealer();
 		Gamer gamer = new Gamer();
 		Rule rule = new Rule();
 		CardDeck cardDeck = new CardDeck();
 		
-		System.out.println(cardDeck.toString());
+		initPhase(cardDeck, gamer);
+		playingPhase(sc, cardDeck, gamer);
+	}
+	
+	// 카드 뽑기
+	public void playingPhase(Scanner sc, CardDeck cardDeck, Gamer gamer) {
+		String gamerInput;
+		
+		while (true) {
+			System.out.println("카드를 뽑으시겠습니까? 종료:0");
+			gamerInput = sc.nextLine();
+			
+			if ("0".equals(gamerInput)) {
+				System.out.println("종료되었습니다.");
+				break;
+			}
+			Card card = cardDeck.draw(); // 뽑고
+			gamer.receiveCard(card); // 받고 확인
+		}
+	}
+	
+	// 처음 시작시 Dealer와 Gamer가 2장씩 카드를 받는 역할을 담당
+	
+	private static final int INIT_RECEIVE_CARD_COUNT = 2;
+	
+	private void initPhase(CardDeck cardDeck, Gamer gamer) {
+		System.out.println("처음 2장의 카드를 각자 뽑겠습니다.");
+		
+		for (int i = 0; i < INIT_RECEIVE_CARD_COUNT; i++) {
+			Card card = cardDeck.draw();
+			gamer.receiveCard(card);
+		}
 	}
 }
--- javaBlackJack/src/main/java/domain/Gamer.java
+++ javaBlackJack/src/main/java/domain/Gamer.java
@@ -1,11 +1,39 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
+/*
+ * 1) 추가로 카드를 받는다.
+ * 2) 뽑은 카드를 소유한다.
+ * 3) 카드를 오픈한다.
+ * Gamer는 현재 카드를 확인 할 수 있어야한다.
+ * */
 public class Gamer {
 
 	private List<Card> cards;
 	
-	public void receiveCard(Card card) {}
-	public List<Card> openCards(){return null;}
+	public Gamer() {
+		cards = new ArrayList<>();
+	}
+	
+	public void receiveCard(Card card) { 
+		this.cards.add(card);
+		this.showCards();// 카드를 받을 때마다 현재 소유한 카드를 확인해야함
+	}
+	
+	public List<Card> openCards(){ // 현재 갖고 있는 모든 카드를 전달하는 역할
+		return this.cards;
+		}
+	
+	public void showCards() {
+		StringBuilder sb = new StringBuilder(); // sysout은 성능저하
+		sb.append("현재 카드 보유 목록 \n");
+		
+		for (Card card : cards) {
+			sb.append(card.toString());
+			sb.append("\n");
+		}
+		System.out.println(sb.toString());
+	}
 }
